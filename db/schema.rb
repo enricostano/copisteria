e849b4e9af8ba292d8dedebb48231041c7b38287
enricostano <ocirne.onats@gmail.com>
2012-08-08 18:23:57 +0200

    created roles_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120808161949) do
+ActiveRecord::Schema.define(:version => 20120808162202) do
 
   create_table "file_projects", :force => true do |t|
     t.string   "name"
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(:version => 20120808161949) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
