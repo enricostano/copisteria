df292e996bda6cb858e027833586166b8bcfe584
enricostano <ocirne.onats@gmail.com>
2012-08-11 13:36:09 +0200

    added splashpage action and views and OpenStreetMap

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @project_institutions = Project.nextbyinstitution
+    if user_signed_in?
+      @project_institutions = Project.nextbyinstitution
+      render "index"
+    else
+      render "splashpage", :layout => "splashpage"
+    end
   end
 end
