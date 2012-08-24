ec8db25ba1fad952a48dc364e2b4657abdedf576
enricostano <ocirne.onats@gmail.com>
2012-08-24 15:08:14 +0200

    added price column to Project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120811152519) do
+ActiveRecord::Schema.define(:version => 20120824130626) do
 
   create_table "carts", :force => true do |t|
     t.boolean  "shipped"
@@ -48,9 +48,10 @@ ActiveRecord::Schema.define(:version => 20120811152519) do
     t.integer  "institution_id"
     t.date     "start"
     t.date     "stop"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.string   "file_uid"
+    t.decimal  "price",          :precision => 8, :scale => 2
   end
 
   create_table "roles", :force => true do |t|
