7959a6bb63767c07bcd8d731465c39fa6e1d3a5b
enricostano <ocirne.onats@gmail.com>
2012-08-24 16:42:20 +0200

    dashboard try 2

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     if user_signed_in?
       @project_institutions = Project.nextbyinstitution
+      @cart = session[:cart]
       render "index"
     else
       render "splashpage", :layout => "splashpage"
