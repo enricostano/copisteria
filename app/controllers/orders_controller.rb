2ff20005cc8836b24ab9621e8fcfe48d4dc00eb9
enricostano <ocirne.onats@gmail.com>
2012-08-28 13:14:40 +0200

    order created notice on dashboard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -62,7 +62,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to :dashboard, notice: "L'ordine è stato creato correttamente." }
         format.json { render json: @order, status: :created, location: @order }
       else
         format.html { render action: "new" }
