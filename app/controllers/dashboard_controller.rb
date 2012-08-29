e2ceaffb4a060b94a49aa77a7b35b1657292e965
enricostano <ocirne.onats@gmail.com>
2012-08-29 16:19:00 +0200

    fixed users role check in dashboard ctrl

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,13 +2,12 @@ class DashboardController < ApplicationController
   def index
     if user_signed_in?
       @project_institutions = Project.nextbyinstitution
-      if current_user.role? :user 
-        @cart = session[:cart] || {}
+      if current_user.any_role? :super_admin, :admin
+		    @orders = Order.order('created_at DESC').limit(10)
+      else
+        @cart = session[:cart] ||= {}
         @orders_by_user = current_user.orders.order('created_at DESC')
       end
-      if current_user.any_role? :SuperAdmin, :Admin
-		@orders = Order.order('created_at DESC').limit(10)
-	  end
       render "index"
     else
       render "splashpage", :layout => "splashpage"
