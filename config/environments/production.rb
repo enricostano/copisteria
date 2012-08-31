bf23ab7277327e51216f45a25c4e01516e0f4aaa
enricostano <ocirne.onats@gmail.com>
2012-08-31 20:42:39 +0200

    added host informations for mailer in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,8 @@ Copisteria2::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => 'www.stanosas.it'}
+  
   # Enable threaded mode
   # config.threadsafe!
 
