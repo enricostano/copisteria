52d91bc78f82dd47de4dfa9097b7ef5a614d00cf
enricostano <ocirne.onats@gmail.com>
2012-09-07 18:30:44 +0200

    trying to get DragonFly working on private folder

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,1 +1,14 @@
-require 'dragonfly/rails/images'
+require 'dragonfly'
+
+app = Dragonfly[:images]
+
+app.configure_with(:rails)
+
+app.define_macro(ActiveRecord::Base, :file_accessor)
+
+app.datastore = Dragonfly::DataStorage::FileDataStore.new
+
+app.datastore.configure do |d|
+  d.root_path = Rails.root.to_s + '/gare'   # defaults to /var/tmp/dragonfly
+  d.server_root = Rails.root.to_s + '/gare'
+end
