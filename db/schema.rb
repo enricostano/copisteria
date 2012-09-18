be2840f4cf961c8408c8f545f4d04986a9bd8e2a
enricostano <ocirne.onats@gmail.com>
2012-09-18 14:47:48 +0200

    modified db schema for Institution model, all attributes > NOT null

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918123727) do
+ActiveRecord::Schema.define(:version => 20120918124441) do
 
   create_table "institutions", :force => true do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.string   "email"
+    t.string   "name",       :null => false
+    t.string   "phone",      :null => false
+    t.string   "email",      :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
