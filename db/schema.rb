b8605ca974caf8bf3d3816035e0bd65fd6a3724e
enricostano <ocirne.onats@gmail.com>
2012-09-18 14:31:31 +0200

    modified db schema for Order model, user_id > NOT null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910185535) do
+ActiveRecord::Schema.define(:version => 20120918122843) do
 
   create_table "institutions", :force => true do |t|
     t.string   "name"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20120910185535) do
   end
 
   create_table "orders", :force => true do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",                       :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.boolean  "authorized", :default => false
