e435d07c9e8cf7ff77617435e8342893988589fa
enricostano <ocirne.onats@gmail.com>
2012-10-27 21:33:59 +0200

    added seeds for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 roles = Role.create([{ name: 'SuperAdmin' }, { name: 'Admin'}, { name: 'User' }])
+users = User.create([{ email: 'enricostn@gmail.com' }, { password: 'password' }, { password_confirmation: 'password'}, { ragionesociale: 'Stano s.a.s.' }, { partitaiva: '12345678901' }, { phone: '675465104' },  { address: 'Carrer Roger de Flor, 297' },  { city: 'Barcelona' }, { cap: '74024' }, { role_ids: '1' }])
