62b0645ae82153af40a6baeac3f6e64df3ba965c
enricostano <ocirne.onats@gmail.com>
2012-11-08 12:48:05 +0100

    added unique index on name field in Roles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108113719) do
+ActiveRecord::Schema.define(:version => 20121108114653) do
 
   create_table "institutions", :force => true do |t|
     t.string   "name",       :null => false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(:version => 20121108113719) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "roles", ["name"], :name => "index_roles_on_name", :unique => true
 
   create_table "roles_users", :id => false, :force => true do |t|
     t.integer "role_id"
