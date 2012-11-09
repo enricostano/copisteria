7b27049e19d3395e79e8cdb63ad1a145b8582de4
enricostano <ocirne.onats@gmail.com>
2012-11-09 15:07:13 +0100

    refactoring User#setup_role

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,17 +23,13 @@ class User < ActiveRecord::Base
 
   before_save :setup_role
 
-  #def role?(role)
-    #return !!self.roles.find_by_name(role.to_s.camelize)
-  #end
-
   def any_role?(*roles)
     return roles.any? { |r| self.roles.map(&:name).include?(r.to_s.camelize) }
   end
 
   def setup_role
     if self.role_ids.empty?
-      self.role_ids = [3]
+      self.role_ids = [Role.find_by_name("User").id]
     end
   end
 end
