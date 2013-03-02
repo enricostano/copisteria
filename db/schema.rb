e416e863679abff57963a3d824e93a6ad6bd5760
enricostano <ocirne.onats@gmail.com>
2013-03-02 15:51:06 +0100

    Migrated DB for new model Volantino

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121109155830) do
+ActiveRecord::Schema.define(:version => 20130302144510) do
 
   create_table "institutions", :force => true do |t|
     t.string   "name",       :null => false
@@ -99,5 +99,13 @@ ActiveRecord::Schema.define(:version => 20121109155830) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["partitaiva"], :name => "index_users_on_partitaiva", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "volantinos", :force => true do |t|
+    t.string   "name"
+    t.date     "start"
+    t.date     "stop"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
