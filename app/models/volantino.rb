89c70d9cb39714cf86247c235204eb77eecc8a79
enricostano <ocirne.onats@gmail.com>
2013-03-03 19:34:06 +0100

    Trying to get this to work

--- app/models/volantino.rb
+++ app/models/volantino.rb
@@ -1,9 +1,9 @@
 class Volantino < ActiveRecord::Base
   include ActiveModel::Validations
-  attr_accessible :name, :start, :stop
+  attr_accessible :name, :start, :stop, :volantino_file, :remove_volantino_file
 
   file_accessor :volantino_file do
-    copy_to(:volantino_thumb){ |a| a[0].thumb('147x198#').png }
+    copy_to(:volantino_thumb){ |a| a.thumb('147x198#').png }
   end
   image_accessor :volantino_thumb
 
--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,10 +1,11 @@
 require 'dragonfly'
 
 app = Dragonfly[:images]
-
+app.configure_with(:imagemagick)
 app.configure_with(:rails)
 
 app.define_macro(ActiveRecord::Base, :file_accessor)
+app.define_macro(ActiveRecord::Base, :image_accessor)
 
 app.datastore = Dragonfly::DataStorage::FileDataStore.new
 
