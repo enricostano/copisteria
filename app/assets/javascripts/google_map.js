cfcd5876fd5fba8b39124aefd7f9ed8e251bf659
enricostano <ocirne.onats@gmail.com>
2013-03-24 15:31:32 +0100

    Added marker to google map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,10 +1,16 @@
 function initialize() {
+  var myLatLng = new google.maps.LatLng(40.388303, 17.634001);
   var mapOptions = {
-    center: new google.maps.LatLng(40.388303, 17.634001),
+    center: myLatLng,
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Stano s.a.s."
+  });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
