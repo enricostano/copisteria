197a13391e5e33b24d88e5d7328155ada5c2e06f
enricostano <ocirne.onats@gmail.com>
2013-04-01 18:11:04 +0200

    Added PNG thumbnail for Volantino PDFs

--- app/models/volantino.rb
+++ app/models/volantino.rb
@@ -3,9 +3,11 @@ class Volantino < ActiveRecord::Base
   attr_accessible :name, :start, :stop, :volantino_file, :remove_volantino_file
 
   file_accessor :volantino_file do
-    copy_to(:volantino_thumb){ |a| a.thumb('147x198#').png }
+    copy_to(:volantino_thumb){ |a| a.thumb('147x').png }
   end
-  image_accessor :volantino_thumb
+  image_accessor :volantino_thumb do
+    after_assign { |a| a.ext = 'png' }
+  end
 
   validates :name, :start, :stop, :presence => true
   validates_with DateValidator
