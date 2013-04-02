3424888d80f4f9dafa1846a7bb0f5743bab4b2cc
enricostano <ocirne.onats@gmail.com>
2013-04-02 15:56:41 +0200

    Added dragonfly processor thumbnail for pdf pages

--- app/models/volantino.rb
+++ app/models/volantino.rb
@@ -3,7 +3,8 @@ class Volantino < ActiveRecord::Base
   attr_accessible :name, :start, :stop, :volantino_file, :remove_volantino_file
 
   file_accessor :volantino_file do
-    copy_to(:volantino_thumb){ |a| a.convert('147x', 'png', '0') }
+    #copy_to(:volantino_thumb){ |a| a.convert('147x', 'png', '0') }
+    copy_to(:volantino_thumb){ |a| a.process(:thumb_pdf_page, '0', '147x', 'png') }
   end
   image_accessor :volantino_thumb do
     after_assign { |a| a.ext = 'png' }
--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -13,3 +13,11 @@ app.datastore.configure do |d|
   d.root_path = Rails.root.to_s + '/gare'   # defaults to /var/tmp/dragonfly
   d.server_root = Rails.root.to_s + '/gare'
 end
+
+app.processor.add :thumb_pdf_page do |temp_object, pdf_page, args = "", format = nil|
+  include Dragonfly::Shell
+  pdf_page_str = pdf_page ? "\[#{pdf_page}\]" : ""
+  binding.pry
+  run "convert", %(#{quote(temp_object.path + pdf_page_str)} #{args} #{quote(temp_object.name)})
+  tempfile
+end
